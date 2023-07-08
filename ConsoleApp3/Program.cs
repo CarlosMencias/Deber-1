d6de216ecdd4ff2a196f2c88a836d924d618fa97
Carlos Mencias <Carlos Mencias@DESKTOP-K9F0DRF>
2023-07-08 17:22:41 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using OOPConcepts.Logica;
+
+try
+{
+    var cliente1 = new Clientes()
+    { 
+        Cliente= "Carlos",
+        Celular=0987330061,
+        Correo= "menciasleal@gmail.com",
+        Huespedes= 5,
+        Pago= "Efectivo",
+        Reserva= new Date(2023,10,12),
+    };
+
+    Console.WriteLine(cliente1);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+Console.WriteLine("Sigo en pie");
---
+++ OOPConcepts.Logica/Clientes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConcepts.Logica
+{
+    public class Clientes
+    {
+        public string? Cliente;
+
+        public int Celular;
+
+        public string? Correo;
+
+        public int Huespedes;
+
+        public string? Pago;
+
+        public Date? Reserva;
+
+        public override string ToString()
+        {
+            return
+                $"Fecha de reserva........:{Reserva, 20}\n" +
+                $"Cliente.................:{Cliente, 20}\n" +
+                $"Celular.................:{Celular, 20}\n" +
+                $"Correo..................:{Correo, 20}\n" +
+                $"Cantidad de huespedes...:{Huespedes, 20}\n" +
+                $"Pago....................:{Pago, 20}\n";
+                
+        }
+    }
+}
